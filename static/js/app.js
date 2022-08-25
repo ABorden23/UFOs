e73538b0a7cd30eed177423b8405f20e6dc9614f
ABorden23 <alexborden2@gmail.com>
2022-08-25 09:39:10 -0400

    app.js change

--- static/js/app.js
+++ static/js/app.js
@@ -40,7 +40,7 @@ function buildTable(data) {
 // }
 
 // 1. Create a variable to keep track of all the filters as an object.
-var filters= {};
+filters= {};
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
